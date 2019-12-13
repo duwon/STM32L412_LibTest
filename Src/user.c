41f0c53d586b874edee8cd4b2bd0bffd32c56e3d
duwon <duwon@duwon.net>
2019-12-14 00:18:14 +0900

    STDIO printf 연동 및 UART 출력

--- Src/user.c
+++ Src/user.c
@@ -1,8 +1,36 @@
+#include <stdio.h>
 #include "user.h"
 #include "gpio.h"
+#include "usart.h"
+
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+PUTCHAR_PROTOTYPE
+{
+  while (HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF) != HAL_OK)
+    ;
+  return ch;
+}
+
+
+static void printUartLogo(void)
+{
+  printf("\r\n");
+  printf("\r\n=============================================");
+  printf("\r\n|           2019         Duwon Jung         |");
+  printf("\r\n|                                           |");
+  printf("\r\n|   HAL Lib Test     %s %s   |",  __DATE__, __TIME__);
+  printf("\r\n=============================================");
+  printf("\r\n\r\n");
+}
 
 void user_start(void)
 {
+	printUartLogo();
   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
 }
 
